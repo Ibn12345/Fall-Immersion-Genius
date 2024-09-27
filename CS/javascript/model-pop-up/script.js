bf782b1cc589e5a4dbdf0282e81138a791b39550
Ibn12345 <ibn.mangum@hgs.hiddengeniusproject.org>
2024-09-26 18:03:47 -0700

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,24 @@
-function modelPopUp() {
-    // Your code here.
-}   
+const modal = document.getElementById("myModal");
+
+// Get Buttons
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const alertBtn = document.getElementById("alertBtn");
+
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+closeModalBtn.onclick = function() {
+     modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    alertBtn.onclick = function() {
+        alert("I like tyler, the creator");
+    }
