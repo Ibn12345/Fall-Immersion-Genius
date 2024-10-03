b156c0b3f09d18a55831ec9214dc934a3e31829f
Ibn12345 <ibn.mangum@hgs.hiddengeniusproject.org>
2024-10-02 17:32:55 -0700

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,5 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+    Image = document.createElement('img');
+    Image.src = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+    document.body.appendChild(Image);
+});
