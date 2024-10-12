0f3bdc1321bd47a263f283d216a531090d3139fa
Ibn Mangum <ibnmangum@TheHiddsProject-531.lan>
2024-10-12 11:55:29 -0700

    saturday push

---
+++ CS/javascript/Hidden Genius Javascript/index.js
@@ -0,0 +1,10 @@
+//console.log("Hello")
+//console.log("I like pizza")
+
+//window.alert("This Is An Alert")
+
+
+document.getElementById("myH1").textContent = "Hello" ;
+document.getElementById("myP").textContent = "I like Pizza!" ;
+document.getElementById("myH2").textContent = "Why do I like Pizza?" ;   
+document.getElementById("myP2").textContent = "Cause its good!" ;   
--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -9,4 +9,3 @@
             (name && email && messsage)
             }
         }
-
